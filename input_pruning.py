333a31ea0c3cbefc20353740e0c4e1b3b9d54a8b
mohammad-mozaffari <mohammad.mozaffari@mail.utoronto.ca>
2023-09-11 13:56:42 -0400

    Added skip_layers to the input pruning method

--- input_pruning.py
+++ input_pruning.py
@@ -1,6 +1,7 @@
 from .m_n_sparsity import Sparsify as MNSparsify
 import torch.nn.functional as F
 from types import MethodType
+from src.utils import is_main_process
 
 
 def sparse_linear_forward(module, input):
@@ -16,11 +17,17 @@ def sparse_linear_activation_forward(module, input):
 
 
 
-def sparsify_inputs(model):
-    print("Modifying Model to Sparse Inputs")
+def sparsify_inputs(model, skip_layers=[]):
+    if is_main_process():
+        print("Modifying Model to Sparse Inputs")
     known_modules = {"Linear", "LinearActivation"}
     for name, module in model.named_modules():
         if type(module).__name__ in known_modules:
+            if module in skip_layers:
+                if is_main_process():   
+                    print("Skipping Module: ", module)
+                continue
+            
             if type(module).__name__ == "Linear":
                 module.forward = MethodType(sparse_linear_forward, module)
             elif type(module).__name__ == "LinearActivation":
