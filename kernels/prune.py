1e92a00dd455d1d08fb9a692900c26f90078f4f7
mohammad-mozaffari <mohammad.mozaffari@mail.utoronto.ca>
2023-10-24 19:10:11 -0400

    Fixed pruning kernel

--- kernels/prune.py
+++ kernels/prune.py
@@ -1,4 +1,4 @@
 from torch.utils.cpp_extension import load
 
 
-pruner = load(name='pruner', sources=['./pruning/kernels/prune.cpp', './pruning/kernels/prune.cu'], verbose=True)
+pruner = load(name='pruner', sources=['./pruning/kernels/prune.cpp', './pruning/kernels/prune.cu'], build_directory="./pruning/kernels/build", verbose=True)
