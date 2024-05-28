df451a559cc2a0914edacf55864318670dfb6f01
Mohammad Mozaffari <mohammad.mozaffari@mail.utoronto.ca>
2024-05-27 18:32:52 -0700

    Added Cedar singularity script

---
+++ scripts/cedar_singularity.sh
@@ -0,0 +1,10 @@
+module load apptainer
+
+rm -rf $SLURM_TMPDIR/torch-one-shot.sif;
+mkdir ${SLURM_TMPDIR}/torch-one-shot.sif;
+tar -xf /home/mozaffar/projects/def-mmehride/mozaffar/torch-one-shot.tar -C $SLURM_TMPDIR;
+mkdir ${SLURM_TMPDIR}/torch-one-shot.sif/etc/pki;
+mkdir ${SLURM_TMPDIR}/torch-one-shot.sif/etc/pki/tls;
+mkdir ${SLURM_TMPDIR}/torch-one-shot.sif/etc/pki/tls/certs;
+cp /etc/ssl/certs/ca-bundle.crt ${SLURM_TMPDIR}/torch-one-shot.sif/etc/pki/tls/certs/ca-bundle.crt;
+singularity shell --bind $PWD:/home/mozaffar --bind $SLURM_TMPDIR:/tmp --nv ${SLURM_TMPDIR}/torch-one-shot.sif
