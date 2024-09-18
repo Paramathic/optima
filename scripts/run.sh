9ad869768ac9d5151ec179f7b42fdf4e75feac46
mohammad-mozaffari <mohammad.mozaffari@mail.utoronto.ca>
2024-09-18 16:37:54 -0400

    Added number of calibration samples

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,75 +8,79 @@ export HF_HOME="data"
 export HF_DATASETS_OFFLINE="1"
 export HF_HUB_OFFLINE="1"
 
-MODEL_PREFIX=meta-llama/Llama-2- #meta-llama/Meta-Llama-3.1- #mistralai/Mistral- #meta-llama/Llama-2- #facebook/opt-
-MODEL_POSTFIX=-hf #-v0.3 #-hf
+MODEL_PREFIX=facebook/opt- #meta-llama/Meta-Llama-3.1- #mistralai/Mistral- #meta-llama/Llama-2- #facebook/opt-
+#MODEL_POSTFIX=-hf #-v0.3 #-hf
 
-for MODEL_SIZE in 7b 13b #125m 350m 1.3b 2.7b 6.7b #13b # 7b #7b 13b #8B #7B #125m # 7b #1.3b #7B #6.7b
+for MODEL_SIZE in 125m #125m 350m 1.3b 2.7b 6.7b #13b # 7b #7b 13b #8B #7B #125m # 7b #1.3b #7B #6.7b
 do
     for STRUCTURE in unstructured 
     do
-        for METHOD in sparsegpt 
+        for METHOD in magnitude
         do
-            for LORA_RANK in 0 #0.1 #0.125 0.25 0.125 0.0625 0.03125 
+            for LORA_RANK in 0.1 #0.1 #0.125 0.25 0.125 0.0625 0.03125
             do
-                for WANDA_IN_LORA in '' #'--wanda_in_lora'
+                for WANDA_IN_LORA in '--wanda_in_lora'
                 do
-                    # rm -rf data
-                    # LOCAL_FILES_ONLY='--local_files_only'
-                    SPARSITY_RATIO=0.5
-                    SHIFT_ZERO_METRICS='--shift_zero_metrics'
-                    EVAL_DATASET='wikitext2'
-                    QUANTIZE='--quantize'
-                    BITWIDTH=4
-                    QUANTIZE_INPUT='--quantize_input'
-                    INPUT_BITWIDTH=8
-                    INPUT_GROUP_SIZE=128
-                    # QUANTIZE_BEFORE_PRUNING='--quantize_before_pruning'
-                    MAX_BITWIDTH=4
-                    USE_STD_IN_QUANTIZATION='--use_std_in_quantization'
-                    #BIAS_CORRECTION='--bias_correction'
-                    EVAL_BATCH_SIZE=1
-                    SEPARATE_LORA='--separate_lora'
-                    UNIFORM_RANK='--uniform_rank'
-                    # ACCELERATE='--accelerate'
-                    # RANDOMIZED_SVD='--randomized_svd'
-                    # LOCAL_CHECKPOINT_DIR='--local_checkpoint_dir llm_weights/flash_attn_gpt2_small_dense_lora0.pt'
-                    TEST_LMHARNESS='--test_lmharness'
-                    # FINE_TUNE='--fine_tune'
-                    EVALUATE_PERPLEXITY='--evaluate_perplexity'
+                    for NUM_CALIBRATION_SAMPLES in 128 #1 2 4 8 16 32 64 128 256
+                    do
+                        # rm -rf data
+                        # LOCAL_FILES_ONLY='--local_files_only'
+                        SPARSITY_RATIO=0.5
+                        SHIFT_ZERO_METRICS='--shift_zero_metrics'
+                        EVAL_DATASET='wikitext2'
+                        QUANTIZE='--quantize'
+                        BITWIDTH=4
+                        QUANTIZE_INPUT='--quantize_input'
+                        INPUT_BITWIDTH=8
+                        INPUT_GROUP_SIZE=128
+                        # QUANTIZE_BEFORE_PRUNING='--quantize_before_pruning'
+                        MAX_BITWIDTH=4
+                        USE_STD_IN_QUANTIZATION='--use_std_in_quantization'
+                        #BIAS_CORRECTION='--bias_correction'
+                        EVAL_BATCH_SIZE=1
+                        SEPARATE_LORA='--separate_lora'
+                        UNIFORM_RANK='--uniform_rank'
+                        # ACCELERATE='--accelerate'
+                        # RANDOMIZED_SVD='--randomized_svd'
+                        # LOCAL_CHECKPOINT_DIR='--local_checkpoint_dir llm_weights/flash_attn_gpt2_small_dense_lora0.pt'
+#                        TEST_LMHARNESS='--test_lmharness'
+                        # FINE_TUNE='--fine_tune'
+                        EVALUATE_PERPLEXITY='--evaluate_perplexity'
 
-                    CUDA_VISIBLE_DEVICES=0 python main_opt.py \
-                        --model ${MODEL_PREFIX}${MODEL_SIZE}${MODEL_POSTFIX} \
-                        --prune_method $METHOD \
-                        --sparsity_ratio $SPARSITY_RATIO \
-                        --sparsity_type $STRUCTURE \
-                        --save out/opt_$MODEL_SIZE/$STRUCTURE/$METHOD/ \
-                        --lora_rank $LORA_RANK \
-                        $WANDA_IN_LORA \
-                        $RANDOMIZED_SVD \
-                        --eval_dataset $EVAL_DATASET \
-                        $SHIFT_ZERO_METRICS \
-                        $LOCAL_CHECKPOINT_DIR \
-                        $QUANTIZE \
-                        $QUANTIZE_BEFORE_PRUNING \
-                        --bitwidth $BITWIDTH \
-                        --max_bitwidth $MAX_BITWIDTH \
-                        $BIAS_CORRECTION \
-                        --bias_alpha 0.3 \
-                        --bias_correction_nsamples 16 \
-                        $USE_STD_IN_QUANTIZATION \
-                        --eval_batch_size $EVAL_BATCH_SIZE \
-                        $SEPARATE_LORA \
-                        $ACCELERATE \
-                        $TEST_LMHARNESS \
-                        --output_csv_path results/group_uniform_opt_perplexity${FINE_TUNE}.csv \
-                        $FINE_TUNE \
-                        $EVALUATE_PERPLEXITY \
-                        $LOCAL_FILES_ONLY \
-                        $QUANTIZE_INPUT \
-                        --input_bitwidth $INPUT_BITWIDTH \
-                        --input_group_size $INPUT_GROUP_SIZE \
-                        $UNIFORM_RANK
+                        CUDA_VISIBLE_DEVICES=0 python main_opt.py \
+                            --model ${MODEL_PREFIX}${MODEL_SIZE}${MODEL_POSTFIX} \
+                            --prune_method $METHOD \
+                            --sparsity_ratio $SPARSITY_RATIO \
+                            --sparsity_type $STRUCTURE \
+                            --save out/opt_$MODEL_SIZE/$STRUCTURE/$METHOD/ \
+                            --lora_rank $LORA_RANK \
+                            $WANDA_IN_LORA \
+                            $RANDOMIZED_SVD \
+                            --eval_dataset $EVAL_DATASET \
+                            $SHIFT_ZERO_METRICS \
+                            $LOCAL_CHECKPOINT_DIR \
+                            $QUANTIZE \
+                            $QUANTIZE_BEFORE_PRUNING \
+                            --bitwidth $BITWIDTH \
+                            --max_bitwidth $MAX_BITWIDTH \
+                            $BIAS_CORRECTION \
+                            --bias_alpha 0.3 \
+                            --bias_correction_nsamples 16 \
+                            $USE_STD_IN_QUANTIZATION \
+                            --eval_batch_size $EVAL_BATCH_SIZE \
+                            $SEPARATE_LORA \
+                            $ACCELERATE \
+                            $TEST_LMHARNESS \
+                            --output_csv_path results/group_uniform_opt_perplexity${FINE_TUNE}.csv \
+                            $FINE_TUNE \
+                            $EVALUATE_PERPLEXITY \
+                            $LOCAL_FILES_ONLY \
+                            $QUANTIZE_INPUT \
+                            --input_bitwidth $INPUT_BITWIDTH \
+                            --input_group_size $INPUT_GROUP_SIZE \
+                            $UNIFORM_RANK \
+                            --nsample $NUM_CALIBRATION_SAMPLES
+                    done
                 done
             done
         done
