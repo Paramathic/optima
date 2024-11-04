484559b50852baba132f49450b333d5d7101e7cf
Mohammad Mozaffari <69392729+Mohammad-Mozaffari@users.noreply.github.com>
2024-11-04 00:34:04 -0500

    Fixed model loading script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,11 +1,11 @@
 export HF_DATASETS_TRUST_REMOTE_CODE="1"
 export HF_HOME="{$SCRATCH}/data"
 
-# python download_data.py --model 'lmharness'
-# python download_data.py --model data
+python utils/download_data.py --model 'lmharness'
+python utils/download_data.py --model data
 
 for MODEL in opt
 do
-    python download_data.py --local_cache --model $MODEL
-    python download_data.py --model $MODEL
+    python utils/download_data.py --local_cache --model $MODEL
+    python utils/download_data.py --model $MODEL
 done
