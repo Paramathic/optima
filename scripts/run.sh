4e88daa3bd39efb6e0b9e652b3f5fef8ebd71380
Mohammad Mozaffari <mohammad.mozaffari@mail.utoronto.ca>
2024-12-29 13:09:47 -0500

    Updated run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -33,75 +33,77 @@ do
 
     for MODEL_SIZE in $MODEL_SIZE_LIST
     do
-        for STRUCTURE in 2:4 unstructured
+        for STRUCTURE in 2:4 #unstructured
         do
-            for METHOD in wanda #joint_pq
+            for METHOD in wanda #sparsegpt #wanda #joint_pq
             do
                 for LORA_RANK in 0.1
                 do
-                    for SLIM_LORA in '--slim_lora'
+                    for SLIM_LORA in '' #'--slim_lora'
                     do
                         for NUM_CALIBRATION_SAMPLES in 128
                         do
                             for QUANTIZE_WEIGHT in '--quantize_weight'
                             do
-                                LOCAL_FILES_ONLY='--local_files_only'
-                                SPARSITY_RATIO=0.5
-                                SHIFT_ZERO_METRICS='--shift_zero_metrics'
-                                EVAL_DATASET='wikitext2'
-                                BITWIDTH=4
-#                                if [ $QUANTIZE_WEIGHT == '--quantize_weight' ]; then
-#                                    QUANTIZE_INPUT='--quantize_input'
-##                                    TILED_INPUT_QUANTIZATION='--tiled_input_quantization'
-#                                fi
-                                INPUT_BITWIDTH=8
-                                INPUT_GROUP_SIZE=128
-                                SLIM_QUANT='--slim_quant'
-                                EVAL_BATCH_SIZE=1
-                                SEPARATE_LORA='--separate_lora'
-                                TEST_LMHARNESS='--test_lmharness'
-                                FINE_TUNE='--fine_tune'
-                                EVALUATE_PERPLEXITY='--evaluate_perplexity'
-                                OPTIMIZER="adafactor"
-#                                PRUNE_LORA="--prune_lora"
-                                QUANTIZE_LORA="--quantize_lora"
-                                LORA_TILE_SIZE=128
-                                # TILED_WEIGHT_QUANTIZATION="--tiled_weight_quantization"
-                                WEIGHT_TILE_SIZE=128
-                                JOINT_PQ_MIXING_FACTOR=2.1
+                                for TILED_WEIGHT_QUANTIZATION in '' #'--tiled_weight_quantization'
+                                do
+                                    LOCAL_FILES_ONLY='--local_files_only'
+                                    SPARSITY_RATIO=0.5
+                                    SHIFT_ZERO_METRICS='--shift_zero_metrics'
+                                    EVAL_DATASET='wikitext2'
+                                    BITWIDTH=4
+    #                                if [ $QUANTIZE_WEIGHT == '--quantize_weight' ]; then
+    #                                    QUANTIZE_INPUT='--quantize_input'
+    ##                                    TILED_INPUT_QUANTIZATION='--tiled_input_quantization'
+    #                                fi
+                                    INPUT_BITWIDTH=8
+                                    INPUT_GROUP_SIZE=128
+                                    SLIM_QUANT='--slim_quant'
+                                    EVAL_BATCH_SIZE=1
+                                    SEPARATE_LORA='--separate_lora'
+                                    TEST_LMHARNESS='--test_lmharness'
+                                    FINE_TUNE='--fine_tune'
+                                    EVALUATE_PERPLEXITY='--evaluate_perplexity'
+                                    OPTIMIZER="adafactor"
+    #                                PRUNE_LORA="--prune_lora"
+                                    # QUANTIZE_LORA="--quantize_lora"
+                                    LORA_TILE_SIZE=128
+                                    WEIGHT_TILE_SIZE=128
+                                    JOINT_PQ_MIXING_FACTOR=2.1
 
-                                CUDA_VISIBLE_DEVICES=3 python main.py \
-                                    --model ${MODEL_PREFIX}${MODEL_SIZE}${MODEL_POSTFIX} \
-                                    --prune_method $METHOD \
-                                    --sparsity_ratio $SPARSITY_RATIO \
-                                    --sparsity_type $STRUCTURE \
-                                    --lora_rank $LORA_RANK \
-                                    $SLIM_LORA \
-                                    --eval_dataset $EVAL_DATASET \
-                                    $SHIFT_ZERO_METRICS \
-                                    $QUANTIZE_WEIGHT \
-                                    --bitwidth $BITWIDTH \
-                                    $SLIM_QUANT \
-                                    --eval_batch_size $EVAL_BATCH_SIZE \
-                                    $SEPARATE_LORA \
-                                    $TEST_LMHARNESS \
-                                    --output_csv_path results/slim-quantizelora-ft.csv \
-                                    $FINE_TUNE \
-                                    $EVALUATE_PERPLEXITY \
-                                    $LOCAL_FILES_ONLY \
-                                    $QUANTIZE_INPUT \
-                                    --input_bitwidth $INPUT_BITWIDTH \
-                                    --input_group_size $INPUT_GROUP_SIZE \
-                                    --nsample $NUM_CALIBRATION_SAMPLES \
-                                    --optimizer $OPTIMIZER \
-                                    $TILED_INPUT_QUANTIZATION \
-                                    $PRUNE_LORA \
-                                    $QUANTIZE_LORA \
-                                    --lora_tile_size $LORA_TILE_SIZE \
-                                    $TILED_WEIGHT_QUANTIZATION \
-                                    --weight_tile_size $WEIGHT_TILE_SIZE \
-                                    $HF_TOKEN \
-                                    --joint_pq_mixing_factor $JOINT_PQ_MIXING_FACTOR
+                                    CUDA_VISIBLE_DEVICES=0 python main.py \
+                                        --model ${MODEL_PREFIX}${MODEL_SIZE}${MODEL_POSTFIX} \
+                                        --prune_method $METHOD \
+                                        --sparsity_ratio $SPARSITY_RATIO \
+                                        --sparsity_type $STRUCTURE \
+                                        --lora_rank $LORA_RANK \
+                                        $SLIM_LORA \
+                                        --eval_dataset $EVAL_DATASET \
+                                        $SHIFT_ZERO_METRICS \
+                                        $QUANTIZE_WEIGHT \
+                                        --bitwidth $BITWIDTH \
+                                        $SLIM_QUANT \
+                                        --eval_batch_size $EVAL_BATCH_SIZE \
+                                        $SEPARATE_LORA \
+                                        $TEST_LMHARNESS \
+                                        --output_csv_path results/wanda-svd-slimquant-ft.csv \
+                                        $FINE_TUNE \
+                                        $EVALUATE_PERPLEXITY \
+                                        $LOCAL_FILES_ONLY \
+                                        $QUANTIZE_INPUT \
+                                        --input_bitwidth $INPUT_BITWIDTH \
+                                        --input_group_size $INPUT_GROUP_SIZE \
+                                        --nsample $NUM_CALIBRATION_SAMPLES \
+                                        --optimizer $OPTIMIZER \
+                                        $TILED_INPUT_QUANTIZATION \
+                                        $PRUNE_LORA \
+                                        $QUANTIZE_LORA \
+                                        --lora_tile_size $LORA_TILE_SIZE \
+                                        $TILED_WEIGHT_QUANTIZATION \
+                                        --weight_tile_size $WEIGHT_TILE_SIZE \
+                                        $HF_TOKEN \
+                                        --joint_pq_mixing_factor $JOINT_PQ_MIXING_FACTOR
+                                done
                             done
                         done
                     done
